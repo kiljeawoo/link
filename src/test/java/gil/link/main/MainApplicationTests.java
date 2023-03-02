ece49ecb37fb63829d03116bca51e88f9a557318
길재우 <6197@.>
2023-03-02 19:55:24 +0900

    Initial commit

---
+++ src/test/java/gil/link/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package gil.link.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
